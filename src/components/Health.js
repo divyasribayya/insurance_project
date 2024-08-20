c06e79bbc0a5e9912304dd4cc8f1d7fd7b324127
DivyasriTCERP <divyasri@techclouderp.com>
2024-08-20 17:50:25 +0530

    first commit message

--- src/components/Health.js
+++ src/components/Health.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const Health = () => {
     return ( 
-    <h1>this is health pagerrrrrrrrrrrrrrrrrreeeeeeeeeeeeeeeee</h1>
+    <h1>this is health page</h1>
      );
 }
 
